bec906f23ed4ad91891ad60f51520d8b218e7dd6
Shon Camarlinghi <shonc.dev@gmail.com>
2026-08-11 14:00:50 -0700

    Revert "Debugging https://github.com/ShonSat/tiger_watch/issues/3"
    This reverts commit 0773c8ed1a0884ea622a4a540f018161570cf01d.

--- convert_ONNX_to_TensoRT.sh
+++ convert_ONNX_to_TensoRT.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/src/tensorrt/bin/trtexec --onnx=best.onnx --saveEngine=best_model.engine --fp16 --verbose
+/usr/src/tensorrt/bin/trtexec --onnx=best.onnx --saveEngine=best_model.engine --fp16
